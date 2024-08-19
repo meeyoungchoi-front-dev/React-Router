23cc8b10c0e5f1ab8acd2a5cf92a4414ed9f8820
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-19 13:05:27 +0900

    상품 상세 페이지 라우트 설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import { Routes, Route } from "react-router-dom";
+import ProductPage from './page/ProductPage';
+import ProductDetailPage from './page/ProductDetailPage';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/about" element={<AboutPage/>}/>
+        <Route path="/products" element={<ProductPage/>}/>
+        <Route path="/products/:id" element={<ProductDetailPage/>}/>
       </Routes>
     </div>
   );
