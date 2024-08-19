ded1b9982b1df92f07e24e6076f410a981946edc
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-19 16:54:28 +0900

    useSeardchParams setQuery 사용하지 않으므로 삭제 및 빈문자열로 초기화

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import './App.css';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route , Navigate} from "react-router-dom";
 import ProductPage from './page/ProductPage';
 import ProductDetailPage from './page/ProductDetailPage';
+import Login from './page/Login';
+import UserPage from "./page/UserPage";
 function App() {
+  const [authenticate, setAuthenticate] = useState(true);
+  const PrivateRoute = () => {
+    return authenticate === true ? <UserPage/> : <Navigate to="/login"/>
+  };
+
+
   return (
     <div className="App">
       <Routes>
@@ -12,6 +21,8 @@ function App() {
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/products" element={<ProductPage/>}/>
         <Route path="/products/:id" element={<ProductDetailPage/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/user" element={<PrivateRoute/>}/>
       </Routes>
     </div>
   );
