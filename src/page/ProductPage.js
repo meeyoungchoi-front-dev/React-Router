e68d44cbb024ea24dee387683b19f0fd83c1540c
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-19 16:34:00 +0900

    query paramter를 가져올 수 있는 useParams

--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const ProductPage = () => {
+  let [query, setQUery] = useSearchParams();
+  console.log("query: ", query.get("page"));
   return (
     <div>
       <h1>Show All Products</h1>
