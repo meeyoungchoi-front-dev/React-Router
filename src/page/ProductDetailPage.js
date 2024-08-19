c48caaf25666e7022287e08b91c04839ff0cdeac
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-19 14:44:35 +0900

    url 파라미터 값을 받는 useParams

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 const ProductDetailPage = () => {
+  const {id}= useParams();
+  console.log("params", id);
   return (
     <div>
-      <h1>Show Product Detail</h1>
+      <h1>Show Product Detail {id}</h1>
     </div>
   )
 }
